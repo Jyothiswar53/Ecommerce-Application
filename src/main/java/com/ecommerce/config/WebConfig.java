80293df57af73a76fa7bbc07fa55ef9c188a8e17
Jyothiswar53 <jyothiswar285@gmail.com>
2025-12-27 12:31:08 +0530

    Serve product images from classpath static uploads

--- src/main/java/com/ecommerce/config/WebConfig.java
+++ src/main/java/com/ecommerce/config/WebConfig.java
@@ -1,6 +1,5 @@
 package com.ecommerce.config;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -8,12 +7,11 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-    @Value("${upload.path}")
-    private String uploadPath;
-
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/productImages/**")
-                .addResourceLocations("file:" + uploadPath + "/");
+
+        registry
+            .addResourceHandler("/productImages/**")
+            .addResourceLocations("classpath:/static/uploads/productimages/");
     }
 }
